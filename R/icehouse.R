bf09c188fc84745c7f0b0a63822a3f559b73d2bb
Trevor L Davis <trevor.l.davis@gmail.com>
2020-02-01 13:07:16 -0800

    style: Tweaks to get 'lint_package' to pass

--- R/icehouse.R
+++ R/icehouse.R
@@ -24,8 +24,10 @@ looneyPyramidGrob <- function(piece_side, suit, rank, cfg = pp_cfg()) { # nolint
     # Circles
     gp_c <- gpar(fill = opt$ps_color, col = opt$ps_color)
     c1_grob <- circleGrob(x = unit(0.5, "npc"), y = unit(0.48, "cm"), r = unit(0.12, "cm"), gp = gp_c)
-    c2_grob <- circleGrob(x = unit(0.5, "npc") - unit(0.38, "cm"), y = unit(0.48, "cm"), r = unit(0.12, "cm"), gp = gp_c)
-    c3_grob <- circleGrob(x = unit(0.5, "npc") - unit(0.76, "cm"), y = unit(0.48, "cm"), r = unit(0.12, "cm"), gp = gp_c)
+    c2_grob <- circleGrob(x = unit(0.5, "npc") - unit(0.38, "cm"),
+                          y = unit(0.48, "cm"), r = unit(0.12, "cm"), gp = gp_c)
+    c3_grob <- circleGrob(x = unit(0.5, "npc") - unit(0.76, "cm"),
+                          y = unit(0.48, "cm"), r = unit(0.12, "cm"), gp = gp_c)
     c_grob <- switch(rank, c1_grob, gList(c1_grob, c2_grob), gList(c1_grob, c2_grob, c3_grob))
 
     # Border
@@ -45,7 +47,7 @@ looneyPyramidGrob <- function(piece_side, suit, rank, cfg = pp_cfg()) { # nolint
 #' @export
 looney_pyramids <- pp_cfg(list(
    n_ranks = 3, n_suits = 6,
-   width.r1.pyramid = 9/16, width.r2.pyramid = 25/32, width.r3.pyramid = 1,
+   width.r1.pyramid = 0.5625, width.r2.pyramid = 0.78125, width.r3.pyramid = 1, # 9/16", 25/32", 1"
    height.r1.pyramid = 1, height.r2.pyramid = 1.375, height.r3.pyramid = 1.75,
    rank_text = ",\u25cf,\u25cf\u25cf,\u25cf\u25cf\u25cf",
    suit_color = "#D55E00,#808080,#009E73,#56B4E9,#E69F00,#808080",
--- tests/testthat/test_icehouse.R
+++ tests/testthat/test_icehouse.R
@@ -4,18 +4,19 @@ library("vdiffr")
 context("looney_pyramids")
 test_that("Looney Pyramids works as expected", {
     # icehouse pyramids
-    dft <- tibble(piece_side="tile_back", x=1.5, y=1.5, suit=NA, rank=NA, angle=NA)
-    dfp <- tibble(piece_side=c("pyramid_face", "pyramid_left", "pyramid_right", "pyramid_back"),
-                  x=c(1,2,2,1), y=c(2,2,1,1), suit=1:4, rank=c(1:3,3), angle=seq(90, 360, 90))
+    dft <- tibble(piece_side = "tile_back", x = 1.5, y = 1.5, suit = NA, rank = NA, angle = NA)
+    dfp <- tibble(piece_side = c("pyramid_face", "pyramid_left", "pyramid_right", "pyramid_back"),
+                  x = c(1, 2, 2, 1), y = c(2, 2, 1, 1), suit = 1:4, rank = c(1:3, 3), angle = seq(90, 360, 90))
     df1 <- rbind(dft, dfp)
 
-    dft <- tibble(piece_side="tile_back", x=3.5, y=3.5, suit=NA, rank=NA, angle=NA)
-    dfp <- tibble(piece_side="pyramid_top", x=2+c(1,2,2,1,1,2,2,2), y=2+c(2,2,1,1,1,1,1,1),
-                  suit=c(1:6,2,3), rank=c(1:3,3,1,2,1,1), angle=seq(0, 630, 90))
+    dft <- tibble(piece_side = "tile_back", x = 3.5, y = 3.5, suit = NA, rank = NA, angle = NA)
+    dfp <- tibble(piece_side = "pyramid_top", x = 2 + c(1, 2, 2, 1, 1, 2, 2, 2),
+                  y = 2 + c(2, 2, 1, 1, 1, 1, 1, 1),
+                  suit = c(1:6, 2, 3), rank = c(1:3, 3, 1, 2, 1, 1), angle = seq(0, 630, 90))
     df2 <- rbind(dft, dfp)
     df <- rbind(df1, df2)
 
     expect_doppelganger("looney_pyramids",
-        function() pmap_piece(df, cfg=looney_pyramids, default.units="in")
+        function() pmap_piece(df, cfg = looney_pyramids, default.units = "in")
     )
 })
