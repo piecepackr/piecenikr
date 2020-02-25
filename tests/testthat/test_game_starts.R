7c1fac9476168001a16fa2d040296ccbfacee53e
Trevor Davis <trevor.l.davis@gmail.com>
2020-02-25 12:31:12 -0800

    tests: Add 'seed' to martian chess tests

--- tests/testthat/test_game_starts.R
+++ tests/testthat/test_game_starts.R
@@ -1,6 +1,6 @@
 library("ppgames")
 test_that("game starts", {
-    verify_output("../text_diagrams/martian_chess_2.txt", cat_piece(df_martian_chess(n_players = 2)))
+    verify_output("../text_diagrams/martian_chess_2.txt", cat_piece(df_martian_chess(seed = 42, n_players = 2)))
     expect_error(df_martian_chess(n_players = 3))
-    verify_output("../text_diagrams/martian_chess_4.txt", cat_piece(df_martian_chess(n_players = 4)))
+    verify_output("../text_diagrams/martian_chess_4.txt", cat_piece(df_martian_chess(seed = 42, n_players = 4)))
 })
