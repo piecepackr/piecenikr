b9f7f5cb3d783c076298ed4cb25677fd8d59da9d
Trevor L Davis <trevor.l.davis@gmail.com>
2021-07-22 08:02:10 -0700

    chore: Skip Unicode board game checks on Windows
    * They seem to choke on Windows but not on Linux/OSX
    * Because the R environment it uses doesn't support Unicode?

--- tests/testthat/test_game_starts.R
+++ tests/testthat/test_game_starts.R
@@ -1,5 +1,6 @@
 library("ppgames")
 test_that("game starts", {
+    skip_on_os("windows")
     verify_output("../text_diagrams/martian_chess_2.txt", cat_piece(df_martian_chess(seed = 42, n_players = 2)))
     expect_error(df_martian_chess(n_players = 3))
     verify_output("../text_diagrams/martian_chess_4.txt", cat_piece(df_martian_chess(seed = 42, n_players = 4)))
